933c4d334b7455ddd288231f4d2a730ccdc28aad
nazim98 <t.nazim1998@gmail.com>
2023-10-30 22:11:04 +0800

    2 update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,33 @@
     @csrf
     <button>Log out</button>
     </form>
+
+    <div style="border: 3px solid black;">
+        <h2>Create a New Post</h2>
+        <form action="/create-post" method="POST">
+        @csrf
+        <input type="text" name="title" placeholder="post title">
+        <textarea name="body" placeholder="body content..."></textarea>
+        <button>Save Post</button>
+        </form>
+    </div>
+
+    <div style="border: 3px solid black;">
+        <h2>All Posts</h2>
+        @foreach($posts as $post)
+        <div style="background-color: grey; padding: 10px; margin: 10px;>
+        <h3>{{$post['title']}} by {{$post->user->name}}</h3>
+        {{$post['body']}}
+        <p><a href="/edit-post/{{ $post->id }}">Edit</a></p>
+        <form action="/delete-post/{{$post->id}}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button>Delete</button>
+        </form>
+        </div>
+        @endforeach
+    </div>
+
     @else
     <div style="border: 3px solid black;">
         <h2>Register</h2>
